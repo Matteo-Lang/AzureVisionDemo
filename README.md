aa0565539b0353f402bd777b42ed68c8d4ce2f46
Matteo Alessandro Lang <matteolang@gmx.de>
2024-07-11 15:47:25 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# AzureVisionDemo
+# Azure Cognitive Services Demo
+
+## Description
+This project implements object recognition using a local webcam and the Computer Vision API from Azure Cognitive Services.
+
+## Running this project
+### Run using a webserver
+The project won't run properly if you just open the LiveCameraResult.html in your browser. Instead, please use a proper web-server.
+
+*Step 1: Install Node.js and npm*
+
+Download and install Node.js from nodejs.org.
+
+*Step 2: Install http-server*
+
+Open your terminal or command prompt and run:
+<code>npm install -g http-server</code>
+
+*Step 3: Start the local server*
+
+Navigate to your project directory and start the server:
+<code>cd /path/to/your/project</code> and then
+<code>http-server</code>
+
+*Step 4: Access your project in the browser*
+
+Open your web browser and go to <code>http://localhost:8080/LiveCameraResult.html</code>.
+
+### Configuring Azure Connection
+To set up a connection with the Cognitive Services API, create a file <code>config.js</code> in the project root with the code below. Fill in the correct endpoint URL and API key into the respective fields. Leave the code otherwise as is.
+<pre><code>// config.js
+export const API_URL = 'https://<span style="color:red">[Endpoint URL]</span>/vision/v3.2/describe'
+export const API_KEY = '<span style="color:red">[API Key]</span>';</code></pre>
---
+++ js/camera.js
@@ -0,0 +1,97 @@
+import { API_URL, API_KEY } from './config.js';
+
+// This is the rate of how frequent your camera image is sent to the Computer Vision API
+var CAPTURE_RATE = 3000;  // milliseconds
+
+// Camera settings
+var CONSTRAINTS = {
+  audio: false,
+  video: { facingMode: 'environment' },  // using the rear camera
+  // video: { facingMode: 'user' }        // using the front camera
+  video: { exact: {
+    height: 500,  // change this to your desired image height
+    width: 800    // change this to your desired image width
+  }}
+};
+
+async function cameraStart() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(CONSTRAINTS);
+
+    var video = document.getElementById('video');
+    video.srcObject = stream;
+    video.play();
+
+    var canvas = document.getElementById('canvas');
+    canvas.style.display = 'none';
+    var context = canvas.getContext('2d');
+
+    setInterval(function () {
+      context.drawImage(video, 0, 0, 680, 480);
+      sendImageToAPI();  // invoke the call to Computer Vision API
+    }, CAPTURE_RATE);
+
+  } catch (err) {
+    console.error('Error starting camera: ', err);
+  }
+}
+
+// Send your camera image to Computer Vision API
+function sendImageToAPI() {
+  var payload = dataURItoBlob();
+  console.log(payload);
+
+  $.ajax({
+    type: 'POST',
+    url: API_URL,
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Ocp-Apim-Subscription-Key': API_KEY
+    },
+    data: payload,
+    processData: false,
+    success: function (data) {
+      console.log(data);
+
+      var captionsResult = '';
+      $.each(data.description.captions, function (index, value) {
+        captionsResult = captionsResult + '<p>' + value.text + ' [confidence: ' + value.confidence + '] </p>';
+      });
+      changeInnerHtml('.captions', captionsResult);
+
+      var tagsResult = '';
+      $.each(data.description.tags, function (index, value) {
+        tagsResult = tagsResult + value + '  ';
+      });
+      changeInnerHtml('.tags', tagsResult);
+    },
+    error: function (xhr, status) {
+      alert('API POST Error.');
+    }
+  });
+}
+
+// Helper functions - DO NOT CHANGE IF YOU DON'T UNDERSTAND THE CODE BELOW
+function dataURItoBlob() {
+  var canvas = document.getElementById('canvas');
+  var dataUri = canvas.toDataURL('image/jpeg');
+  var data = dataUri.split(',')[1];
+  var mimeType = dataUri.split('')[0].slice(5);
+
+  var bytes = window.atob(data);
+  var buf = new ArrayBuffer(bytes.length);
+  var byteArr = new Uint8Array(buf);
+
+  for (var i = 0; i < bytes.length; i++) {
+    byteArr[i] = bytes.charCodeAt(i);
+  }
+  return byteArr;
+}
+
+function changeInnerHtml(elementPath, newText) {
+  $(elementPath).fadeOut(500, function () {
+    $(this).html(newText).fadeIn(500);
+  });
+}
+
+export { cameraStart };
